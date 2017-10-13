af62cd975ec3243e6533671aaa1baa1fd4eb02d9
steven <zhaofei.steven@gmail.com>
2017-10-12 22:56:30 -0400

    Add a table prototype.

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -5,10 +5,336 @@ import Calendar from 'rc-calendar';
 import logo from './logo.svg';
 import './App.css';
 
-import { Button } from 'semantic-ui-react'
+import { Button, Grid, Icon, Label, Menu, Table } from 'semantic-ui-react'
 
-//export default ButtonExampleButton
+const TableDef = () => (
+  <Table celled striped structured>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell width={2} />
+        <Table.HeaderCell>Study Space 1</Table.HeaderCell>
+        <Table.HeaderCell>Study Space 2</Table.HeaderCell>
+        <Table.HeaderCell>Study Space 3</Table.HeaderCell>
+        <Table.HeaderCell>Study Space 4</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
 
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>12:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>1:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>2:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>3:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>4:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>5:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>6:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>7:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>8:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>9:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>10:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>11:00 AM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>12:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>1:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>2:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>3:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>4:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>5:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>6:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>7:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>8:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>9:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>10:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell rowSpan='2'>11:00 PM</Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+        <Table.Cell selectable></Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+)
 
 class App extends Component {
   render() {
@@ -18,24 +344,35 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Felis Resource Management System</h1>
         </header>
-            <Button>Login</Button>
+        <Button>Login</Button>
+        <Menu floated='right' pagination>
+        <Menu.Item as='a' icon>
+          <Icon name='left chevron' />
+        </Menu.Item>
+        <Menu.Item as='a'>1</Menu.Item>
+        <Menu.Item as='a'>2</Menu.Item>
+        <Menu.Item as='a'>3</Menu.Item>
+        <Menu.Item as='a'>4</Menu.Item>
+        <Menu.Item as='a' icon>
+          <Icon name='right chevron' />
+        </Menu.Item>
+      </Menu>
         {/*<p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
          </p>*/}
 
-         <div class="ui celled grid">
-         <div class="row">
-           <div class="three wide column">
-               <p>Find available resource today.</p>
-               <Calendar />
-               
-           </div>
-           <div class="thirteen wide column">
-             <img class="ui image" src="/assets/images/wireframe/centered-paragraph.png" />
-           </div>
-         </div>         
-       </div>
-        
+        <Grid celled>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <p>Find available resource today.</p>
+              <Calendar />
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <TableDef />
+            </Grid.Column>
+          </Grid.Row>
+
+        </Grid>
       </div>
     );
   }
